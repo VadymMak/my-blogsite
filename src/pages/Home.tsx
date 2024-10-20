f879fca8fe1a00da5a8392cc1980447929107b84
VadymMak <makevytssvadym@gmail.com>
2024-10-20 09:07:02 +0200

    Fix build settings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ const Home = () => {
         <h2>Our Location</h2>
         <div className={styles.mapContainer}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20994.971545197128!2d18.0472588!3d48.8701952!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4714a3d19dfeb9cb%3A0xd8835da1e686658f!2sOC%20Ju%C5%BEanka!5e0!3m2!1sru!2ssk!4v1729228781380!5m2!1sru!2ssk"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93031.0181942096!2d27.78026205034398!3d43.2258611704557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a45439205714b3%3A0x99c4e0902fa8939b!2s9010%20Varna%2C%20Bulgaria!5e0!3m2!1sen!2spl!4v1729407709833!5m2!1sen!2spl"
             width="600"
             height="450"
             style={{ border: 0 }}
