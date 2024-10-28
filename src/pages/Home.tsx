9f7b6eebae33955d07b3bee07e8064f71696b648
VadymMak <makevytssvadym@gmail.com>
2024-10-28 08:55:23 +0100

    About us section upgraded_1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.scss";
 
 const Carousel = React.lazy(() => import("../components/Carousel"));
 const Contacts = React.lazy(() => import("../components/Contacts"));
-const PhoneIcon = React.lazy(() => import("../components/PhoneIcon"));
+// const PhoneIcon = React.lazy(() => import("../components/PhoneIcon"));
 const AboutUs = React.lazy(() => import("./AboutUs"));
 const Banner = React.lazy(() => import("../components/Banner"));
 const OurLocation = React.lazy(() => import("../components/OurLocation"));
