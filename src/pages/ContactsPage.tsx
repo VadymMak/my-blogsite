7e03cfa4f608895d62779354c3faa4191fe33644
Vadym Mak <makevytssvadym@gmail.com>
2026-02-08 18:52:51 +0300

    fix: fixed contact map

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -31,37 +31,41 @@ const ContactsPage: React.FC = () => {
 
       <section>
         <h2>{t("officeHours")}</h2>
-        <p>{t("weekdays")}</p>
-        <p>{t("weekends")}</p>
+        <p>{t("officeHoursText")}</p>
+        <p>{t("mondayToFriday")}</p>
+        <p>{t("saturday")}</p>
+        <p>{t("sunday")}</p>
+      </section>
+
+      <section>
+        <h2>{t("findUsHere")}</h2>
+        <iframe
+          className={styles.map}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93031.0181942096!2d27.78026205034398!3d43.2258611704557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a45439205714b3%3A0x99c4e0902fa8939b!2s9010%20Varna%2C%20Bulgaria!5e0!3m2!1sen!2spl!4v1729407709833!5m2!1sen!2spl"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          title="Google Map - UB Market Location"
+        />
       </section>
 
       <section>
         <h2>{t("followUs")}</h2>
         <ul>
           <li>
-            <a
-              href="https://www.instagram.com/ub_market_ltd"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://www.instagram.com/ub_market_ltd" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faInstagram} />
             </a>
           </li>
           <li>
-            <a
-              href="https://t.me/ub_market_ltd"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://t.me/ub_market_ltd" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faTelegram} />
             </a>
           </li>
           <li>
-            <a
-              href="https://wa.me/+359884469860"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://wa.me/+359884469860" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faWhatsapp} />
             </a>
           </li>
